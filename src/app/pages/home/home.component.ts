6e283894f9463441f1c9a27be533cf1c08ceabbc
bengin34 <becaglar3434@gmail.com>
2024-04-24 10:03:52 +0200

    header

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { ProductsHeaderComponent } from './components/products-header/products-header.component';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [MatSidenavModule, ProductsHeaderComponent],
+ templateUrl: './home.component.html',
+})
+export class HomeComponent {
+
+}
