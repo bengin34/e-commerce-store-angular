5722c3820ead34ec5bcdc6d7e89ea2cfb3f9bd87
bengin34 <becaglar3434@gmail.com>
2024-04-25 14:40:30 +0200

    cart component routes

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -4,12 +4,13 @@ import { MatIconModule } from "@angular/material/icon";
 import { MatBadgeModule } from "@angular/material/badge";
 import { MatMenuModule } from "@angular/material/menu";
 import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
 
 
 @Component({
   selector: "app-header",
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatMenuModule, MatBadgeModule, CommonModule],
+  imports: [MatToolbarModule, MatIconModule, MatMenuModule, MatBadgeModule, CommonModule, RouterModule],
   templateUrl: "./header.component.html",
 })
 export class HeaderComponent {}
--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -1,16 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Cart, CartItem } from "../../models/cart.model";
+import { MatCardModule } from "@angular/material/card";
+import { CommonModule } from "@angular/common";
+import { MatButtonModule } from "@angular/material/button";
+import { RouterModule } from "@angular/router";
+
 
 @Component({
-  selector: 'app-cart',
+  selector: "app-cart",
   standalone: true,
-  imports: [],
-  template: `
-    <p>
-      cart works!
-    </p>
-  `,
-  styles: ``
+  imports: [MatCardModule,CommonModule,MatButtonModule, RouterModule],
+  templateUrl: "./cart.component.html",
 })
-export class CartComponent {
+export class CartComponent implements OnInit {
+  cart: Cart = {
+    items: [
+      // {
+      //   product: "https=//via.placeholder.com/150",
+      //   name: "snickers",
+      //   price: 150,
+      //   quantity: 1,
+      //   id: 1,
+      // },
+    ],
+  };
 
+  dataSource: Array<CartItem> = [];
+  displayedColums: Array<string> = [
+    "product",
+    "name",
+    "price",
+    "quantity",
+    "total",
+    "action",
+  ];
+
+  ngOnInit(): void {
+    this.dataSource = this.cart.items;
+  }
 }
